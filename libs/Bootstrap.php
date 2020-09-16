829f6b89bebb5676c7428d1e32d4ab4f5cc87942
Michael Joshua Marana <mjd.marana@gmail.com>
2020-09-16 16:21:52 +0800

    url

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -27,7 +27,7 @@ class Bootstrap
       $this->error();
       return false;
     }
-    $controller = new $controller();
+    $controller = new HomeController();
     // $controller->loadModel($url[0]);
 
 
